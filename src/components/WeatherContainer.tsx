aea113a2de6bdced58629507f1852e77058f01ae
Karol <kypacheco07@misena.edu.co>
2022-09-09 16:12:33 -0500

    Declaring values

--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -1,6 +1,47 @@
-export const WeatherContainer = () => {
+import { useEffect, useState } from "react";
+
+export const WeatherContainer = ({
+  fetchedData,
+  error,
+}: {
+  fetchedData: any;
+  error: string;
+}) => {
+  const [weather, setWeather] = useState({
+    city: "",
+    country: "",
+    temperature: 0,
+    description: "",
+    icon: "",
+    humidity: "",
+    feels: "",
+    visibility: "",
+    pressure: "",
+    longitude: "",
+    latitude: "",
+    windSpeed: "",
+  });
+
+  useEffect(() => {
+    if (fetchedData)
+      setWeather({
+        city: fetchedData.name,
+        country: fetchedData.sys.country,
+        temperature: Math.floor(fetchedData.main.temp - 273),
+        description: fetchedData.weather[0].description,
+        icon: ``,
+        humidity: fetchedData.main.humidity + "%",
+        feels: Math.floor(fetchedData.main.feels_like - 273) + "°C",
+        visibility: fetchedData.visibility + "m",
+        pressure: fetchedData.main.pressure + "hPa",
+        longitude: fetchedData.coord.lon,
+        latitude: fetchedData.wind.speed + "m/s",
+        windSpeed: fetchedData.wind.speed + "m/s",
+      });
+  }, [fetchedData]);
+
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center p-3">
       <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-stone-500 shadow-lg">
         <img
           className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
